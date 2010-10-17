8cfcdd16c2eb7f0ce0aa19ba65686921d21613e8
Arthur Furlan <afurlan@afurlan.org>
2010-10-17 17:28:05 -0200

    Add the "CREATE UNIQUE INDEX" command to create an index on "url" column.

--- src/shortim/sql/shorturl.postgresql_psycopg2.sql
+++ src/shortim/sql/shorturl.postgresql_psycopg2.sql
@@ -1,2 +1,3 @@
 -- change the serial column to use "bigint" instead of "int"
 ALTER TABLE shortim_shorturl ALTER COLUMN id TYPE bigint;
+CREATE UNIQUE INDEX shortim_shorturl_url_idx ON shortim_shorturl (url);
