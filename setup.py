cc68d3850f67c0341c8bcd9fd82d5e580d40a652
Arthur Furlan <afurlan@afurlan.org>
2011-04-20 23:09:20 -0300

    Upgrade version number to 1.2.3.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(fname):
 
 setup(
     name="django-shortim",
-    version="1.2.2",
+    version="1.2.3",
     url='http://github.com/valvim/django-shortim',
     license='GPLv3',
     description=("Django application for creating short URLs. "
